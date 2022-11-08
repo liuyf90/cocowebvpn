56b7eac6d5eea2e00f97794af0e3570eef705386
liuyf90 <liuyf90@gmail.com>
2022-11-08 16:40:10 +0800

    add get_sub_domain function

---
+++ lua/lib/domain.lua
@@ -0,0 +1,26 @@
+--[[
+  Author:liuyf
+  Email:liuyf90@gmail.com
+  Time:Nov 8, 2022 at 16:40
+
+  ngx.var.xx  xx is nginx's inner variable， ex: $host
+  According to the pertinent internet recommendations (RFC3986 section 2.2)
+--]]
+
+local _M={}
+function _M.get_sub_domain()
+    local subdomain = "([A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])?)"  
+    local m, err = ngx.re.match(ngx.var.host, subdomain, "i")
+    local ngx_say = ngx.say
+    if not m then
+        ngx.log(ngx.ERR, "error: ", err)
+        return
+    end
+    --the life time identical to the current request 
+    ngx.ctx.domain = m[0]
+    ngx_say(ngx.ctx.domain)
+ end
+
+return _M
+
+

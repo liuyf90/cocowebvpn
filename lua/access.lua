915e0b916abd35bbdafbef512f7b09b23f680828
liuyf90 <liuyf90@gmail.com>
2022-11-11 16:46:24 +0800

    update nginx.conf and replace ; change phase order

--- lua/access.lua
+++ lua/access.lua
@@ -10,13 +10,13 @@ local ips = ngx.shared.ips
 local ip = ngx.var.remote_addr
 local function proxy_to()
     local res = ngx.location.capture("/download_server",
-             { share_all_vars = true });
+             { copy_all_vars = true,always_forward_body = true })
     if res.status ~= ngx.HTTP_OK then
     ngx.log(ngx.INFO, "get sub_rquest res.status:"..res.status)
         ngx.say('Failed to process, please try again in some minutes.')
-        ngx.exit(403);
+        ngx.exit(403)
     end
-    ngx.print(res.body)
+    ngx.say(res.body)
 end
 
 
@@ -24,6 +24,8 @@ end
 if ips:get(ip) then
     ngx.var.proxy = "127.0.0.1:7777"--..ngx.var.uri
     ngx.log(ngx.INFO, "access-proxy-ip: "..ngx.var.proxy)
+    ngx.req.read_body() --will be directly forwarded to the subrequest without copying the whole request body data when creating the subrequest
+
     proxy_to()
 end
 
--- lua/replace.lua
+++ lua/replace.lua
@@ -1,25 +1,39 @@
+--[[
+    @author liuyf
+    Time:Nov 10, 2022 at 15:17
+
+    replace the content of the resp body  returned by the sub-request
+--]]
+
 ngx.log(ngx.INFO,"replace resp body :")
 -- body_filter_by_lua, body filter模块，ngx.arg[1]代表输入的chunk，ngx.arg[2]代表当前chunk是否为last
 local chunk, eof = ngx.arg[1], ngx.arg[2]
-local buffered = ngx.ctx.buffered
-if not buffered then
-   buffered = {}  -- XXX we can use table.new here 
-   ngx.ctx.buffered = buffered
+-- 定义全局变量，收集全部响应
+if ngx.ctx.buffered == nil then
+    ngx.ctx.buffered = {}
 end
-if chunk ~= "" then
-   buffered[#buffered + 1] = chunk
-   ngx.arg[1] = nil
-end
-if eof then
-   local whole = table.concat(buffered)
-   ngx.ctx.buffered = nil
-   -- try to unzip
-   -- local status, debody = pcall(com.decode, whole) 
-   -- if status then whole = debody end
-   -- try to add or replace response body
-   -- local js_code = ...
-   -- whole = whole .. js_code
-   whole = string.gsub(whole, "max%-width%:1632px%;",  "")
-   ngx.arg[1] = whole
-end
+-- 如果非最后一次响应，将当前响应赋值
+if chunk ~= "" and not ngx.is_subrequest then
+    table.insert(ngx.ctx.buffered, chunk)
 
+    -- 将当前响应赋值为空，以修改后的内容作为最终响应
+    ngx.arg[1] = nil
+end
+-- 如果为最后一次响应，对所有响应数据进行处理
+if eof then
+    -- 获取所有响应数据
+    local whole = table.concat(ngx.ctx.buffered)
+    ngx.ctx.buffered = nil
+
+    -- 进行你所需要进行的处理
+    -- try to unzip
+    -- local status, debody = pcall(com.decode, whole) 
+    -- if status then whole = debody end
+    -- try to add or replace response body
+    -- local js_code = ...
+    -- whole = whole .. js_code
+    --whole = string.gsub(whole, "max%-width%:1632px%;",  "")
+
+    -- 重新赋值响应数据，以修改后的内容作为最终响应
+    ngx.arg[1] = whole
+end

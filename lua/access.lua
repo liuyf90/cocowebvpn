bae5914a4e5aa6e25e92599b299839aa70d34936
liuyf90 <liuyf90@gmail.com>
2022-11-12 10:40:16 +0800

    add resp.lua whith packing some function

--- lua/access.lua
+++ lua/access.lua
@@ -16,16 +16,33 @@ local function proxy_to()
         ngx.say('Failed to process, please try again in some minutes.')
         ngx.exit(403)
     end
-    ngx.say(res.body)
+   ngx.say(res.body)
 end
 
+
+function print_req_headers()
+    local h, err = ngx.req.get_headers()
+
+    if err == "truncated" then
+        -- one can choose to ignore or reject the current request here
+    end
+    local i=0;
+    for k, v in pairs(h) do
+       i=i +1
+       ngx.log(ngx.INFO,"req_header["..i.."]:"..k..':'..v)
+    end
+end
 
 -- this is target ip or domainName ,proxy these in here 
 if ips:get(ip) then
     ngx.var.proxy = "127.0.0.1:7777"--..ngx.var.uri
     ngx.log(ngx.INFO, "access-proxy-ip: "..ngx.var.proxy)
     ngx.req.read_body() --will be directly forwarded to the subrequest without copying the whole request body data when creating the subrequest
-
+    print_req_headers()
     proxy_to()
 end
+
+
+
+
 

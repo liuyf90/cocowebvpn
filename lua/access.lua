0b39e13b88b4549867934ddcd5026b6c97e1418c
liuyf90 <liuyf90@gmail.com>
2022-11-23 11:17:47 +0800

    successfully  resovled capture issues

--- lua/access.lua
+++ lua/access.lua
@@ -9,42 +9,51 @@
 local ips = ngx.shared.ips
 local ip = ngx.var.remote_addr
 local url = "/download_server"..ngx.var.request_uri
+local my_scheme='http'
+local my_host='proxy.com:8888'
+local ziel= my_scheme.."://"..my_host
 ngx.log(ngx.INFO,"****url****="..url)
 
 local function proxy_to()
-    local method = nil 
-    if ngx.req.get_method() == "GET" then
-       method = ngx.HTTP_GET 
-    end
-    if ngx.req.get_method() == "POST" then
-       method = ngx.HTTP_POST
-    end
-    local res = ngx.location.capture(url,
-             {method = mehtod, copy_all_vars = true,always_forward_body = true })
+    local map = {
+        GET = ngx.HTTP_GET,
+        POST = ngx.HTTP_POST,
+    }
+    local res = ngx.location.capture('/redirect_checker',
+    {method = map[ngx.var.request_method], body = ngx.var.request_body})
     ngx.log(ngx.INFO,"localtion.capture status: "..res.status)
     --set sub restquest header
     for k, v in pairs(res.header) do
-           ngx.header[k] = v
+        ngx.header[k] = v
     end
 
-    --if res.status ~= ngx.HTTP_OK then
-  --  if (300<res.status) and (res.status<200) then
-  --
-  --      ngx.log(ngx.INFO, "get sub_rquest res.status:"..res.status)
-  --      ngx.say('Failed to process, please try again in some minutes.')
-  --      ngx.exit(403)
-  --  end
-   ngx.say(res.body)
+    --Detect/change redirect
+    local redirect_target = res.header.Location
+    if redirect_target and res.status > 300 and res.status <309 then
+        ngx.log(ngx.ALERT,'redirect_target:'..redirect_target)
+        local redirect_target_changed,n,err=ngx.re.gsub(redirect_target, '^https?[:][/][/][^/]+', ziel)
+        ngx.log(ngx.ALERT, 'redirect_target_changed: '..redirect_target_changed)
+        return ngx.redirect(redirect_target_changed, 303)
+    elseif res.status == 500 then
+        return ngx.exit(500)
+    else
+        ngx.exec('@download_server')
+        return ngx.exit(ngx.HTTP_OK)
+    end
+
 end
+
+   --ngx.say(res.body)
 
 -- this is target ip or domainName ,proxy these in here 
 if ips:get(ip) then
     ngx.var.proxy = '218.9.68.192:8093'
-    proxy_to()
     ngx.log(ngx.INFO, "access-proxy-ip: "..ngx.var.proxy)
     ngx.req.read_body() --will be directly forwarded to the subrequest without copying the whole request body data when creating the subrequest
     local req = require "lua.lib.req"
     req.print_req_headers()
+    req.set_req_body_data()
+    proxy_to()
 
 end
 

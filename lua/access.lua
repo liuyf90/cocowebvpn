341fafb7fb5de89e6906aa7b1a603212df8244fe
liuyf90 <liuyf90@gmail.com>
2022-12-04 11:41:57 +0800

    Edit access.lua to add capture domain name form sub_json

--- lua/access.lua
+++ lua/access.lua
@@ -32,31 +32,27 @@ for _,v in ipairs(keys) do
     local json = cjson.decode(v)
     local sub_json=cjson.decode(json)
     local t = sub_json["oa.webvpn.com"] 
+    --ngx.log(ngx.ALERT,"*****$sub_json:"..type(sub_json))
+    --ngx.log(ngx.ALERT,"*****$proxy="..t.web)
     if t then
-         --ngx.var.proxy = t.url..':'..t.port  
---         ngx.var.proxy = t.web
- --        ngx.log(ngx.ALERT,"*****$proxy="..t.web)
-         ngx.var.proxy = 'oa.hrbfu.edu.cn'
-         proxy_to()
+           --ngx.var.proxy = 'oa.hrbfu.edu.cn'
+           --capture domainName form t.web
+           local captures, err =ngx.re.match(t.web,"[^/]+","jo")
+           if captures then
+               local domainName= captures[0]
+               --ngx.log(ngx.ALERT,"*****$domainName="..domainName)
+               ngx.var.proxy = domainName
+               proxy_to()
+           else
+               if err then
+                   ngx.log(ngx.ERR, "error: ", err)
+                   return
+               end
+             return 
+           end           
     end
 end
 
-
--- this is target ip or domainName ,proxy these in here 
---if ips:get(web) then
---    ngx.var.proxy = '218.9.68.192:8093'
---    ngx.req.read_body() --will be directly forwarded to the subrequest without copying the whole request body data when creating the subrequest
---    proxy_to()
---end
---local json =[[ {"oa.webvpn.com":["10.49.2.5","8080","oa.web.com"]} ]]
---ngx.log(ngx.ALERT,"****json="..json)
---local key = cjson.decode(json)
---ngx.log(ngx.ALERT,"*****key="..key["oa.webvpn.com"][1])
-
-
---local json =[[ {"oa.webvpn.com":{"url":"10.49.2.5","port":"8080","web":"oa.web.com"}} ]]
---local sub_json=cjson.decode(json)["oa.webvpn.com"]
---ngx.log(ngx.ALERT,"*****key="..sub_json.url)
 
 
 

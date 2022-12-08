b07f6484668de78935d2db318fe67bd7297d6fa8
liuyf90 <liuyf90@gmail.com>
2022-12-08 13:58:09 +0800

    Add variable cas in init_data and nginx.conf

--- lua/init_data.lua
+++ lua/init_data.lua
@@ -7,8 +7,9 @@
 
 local _M = {} 
 local data = { 
-    casport = 7777, 
-    domainName = 'proxyman.com:8888',
+    casport = 7777, --代理的cas端口号
+    domainName = 'proxyman.com:8888', --busi模块的servername
+    cas = "ids.hrbfu.edu.cn" --cas服务的原始地址
 } 
 function _M.get(name) 
   return data[name] 

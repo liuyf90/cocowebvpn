7b368f98727ee92a8022182ef59885fc7fb2bb27
liuyf90 <liuyf90@gmail.com>
2022-12-27 10:59:36 +0800

    fix some bug

--- lua/resp_header_filter.lua
+++ lua/resp_header_filter.lua
@@ -1,6 +1,7 @@
 --[[
     @author liuyf
     Time:Nov 25, 2022 at 11:17
+    edit by dec 27 ,2022
 
     repalce resp header
     Rewrite location header when status is 3xx and location header is present
@@ -27,9 +28,11 @@ if redirect_target and ngx.status > 300 and ngx.status <309 then
     --ngx.log(ngx.ALERT,'location='..redirect_target)
     --ngx.log(ngx.ALERT,'redirect_target:'..redirect_target)
     local data = require "lua.init_data"
+    --如果$1等于subdomainname，例如：web2vpn==web2vpn 那么只有一种情况就是此时的请求是来自于门户webvpn2.hrbfu.edu.cn,此时取它的子域名就是webvpn2,因为此时没有子域名。
     local redirect_target_changed,n,err=ngx.re.gsub(redirect_target, '^(https?)[:][/][/][^/]+', '$1://'..subdomain..data.get("domainName"))
+    local redirect_target_changed_riddouble,n,err=ngx.re.gsub(redirect_target_changed, [[(\w+\.)(\1)]], '$2')
     --ngx.log(ngx.ALERT, 'redirect_target_changed: '..redirect_target_changed)
-    ngx.header.Location = redirect_target_changed
+    ngx.header.Location = redirect_target_changed_riddouble
 elseif ngx.status == 500 then
     return ngx.exit(500)
 end
